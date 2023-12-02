be9356e09a4126733642701d6b740d9b3fb1ded4
Bradley Black <bradley_black@me.com>
2023-12-03 11:05:39 +1300

    Finished palindrome fortran example

--- examples/palindrome.f95
+++ examples/palindrome.f95
@@ -1,6 +1,9 @@
 program palindrome
     character (len=255) word1
     character (len=255) word2
+    integer :: word1_size
+    integer :: word2_size
+    logical :: palindromes
 
     ! Get user input
     write(*, '(a)', advance='no') "Enter word1: "
@@ -8,4 +11,28 @@ program palindrome
 
     write(*, '(a)', advance='no') "Enter word2: "
     read *, word2
+
+    word1_size = len(trim(word1))
+    word2_size = len(trim(word2))
+
+    palindromes = .TRUE.
+
+    if (word1_size == word2_size) then
+        do i = 1, word1_size
+            if (word1(i:i) == word2(word2_size:word2_size)) then
+                word2_size = word2_size - 1
+            else
+                palindromes = .FALSE.
+            end if
+        end do
+    else
+        palindromes = .FALSE.
+    end if
+
+    if (palindromes .eqv. .TRUE.) then
+        print *, "These are palindromes!"
+    else
+        print *, "These are not palindromes!"
+    end if
+
 end program palindrome
