b84106ef068632e605c1786b3dcd149cbd75161f
Bradley Black <bradley_black@me.com>
2023-12-03 11:05:39 +1300

    Trim empty characters from name in hello.f95

--- examples/hello.f95
+++ examples/hello.f95
@@ -2,5 +2,5 @@ program hello
     character(len=255) :: given_name
     write (*, '(a)', advance='no') "What is your name?"
     read *, given_name
-    print *, "Hello ", given_name, "!"
+    print *, "Hello ", trim(given_name), "!"
 end program hello
