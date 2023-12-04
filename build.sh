13d8d69dfe53f34c816ab6ac7728a608c33f616e
Bradley Black <bradley_black@me.com>
2023-12-04 18:54:29 +1300

    Build and clean tests from build.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,12 @@ if [ $1 == "build" ]; then
     $DOCKER_COMMAND sh -c "/inside.sh build"
 elif [ $1 == "examples" ]; then
     $DOCKER_COMMAND sh -c "/inside.sh examples"
+elif [ $1 == "test" ]; then
+    $DOCKER_COMMAND sh -c "/inside.sh test"
 elif [ $1 == "clean" ]; then
     $DOCKER_COMMAND sh -c "/inside.sh clean"
+elif [ $1 == "cleantest" ]; then
+    $DOCKER_COMMAND sh -c "/inside.sh cleantest"
 elif [ $1 == "run" ]; then
     $DOCKER_COMMAND sh -c "/inside.sh run"
 elif [ $1 == "shell" ]; then
